66daf9314bd7ce33071599e9d0b5c66cd166979e
Lenilson Nascimento <lenilson@Lenilson-Mac.local>
2016-02-16 20:39:49 -0200

    Implement tag search

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,8 +5,17 @@ var searchEngine = {
     this.field('id');
     this.field('title', { boost: 10 });
     this.field('author');
-    this.field('description');
+    this.field('description', { boost: 5 });
     this.field('tags');
+    this.field('url');
+  }),
+
+  index_tag: lunr(function () {
+    this.field('id');
+    this.field('title');
+    this.field('author');
+    this.field('description');
+    this.field('tags', { boost: 10 });
     this.field('url');
   }),
 
@@ -16,6 +25,9 @@ var searchEngine = {
     searchEngine.data.then(function(loaded_data){
       $.each(loaded_data, function(index, value){
         searchEngine.index.add(
+          $.extend({ "id": index }, value)
+        );
+        searchEngine.index_tag.add(
           $.extend({ "id": index }, value)
         );
       });
@@ -33,14 +45,24 @@ var searchEngine = {
     }
   },
 
-  perform: function(query){
-    var results = searchEngine.index.search(query);
+  perform: function(query,tag=false){
+    searchEngine.populate();
+    var results = null;
+    var msg_success = null;
+    var msg_error = null;
+    
+    if(tag === true) {
+      results = searchEngine.index_tag.search(query);
+    } else {
+      results = searchEngine.index.search(query);
+    }
+
     searchEngine.data.then(function(loaded_data){
       if(results.length) {
         searchEngine.post_list.empty();
         results.forEach(function(result) {
           var item = loaded_data[result.ref];
-          var appendString = '<h1>Showing result for "'+query+'"</h1>'+
+          var appendString = '<h1>Showing results for "'+query+'"</h1>'+
           '<article class="post">'+
           '<h1 class="title">' + item.title + '</h1>'+
           '<p class="description">' + item.description + '</p>'+
@@ -55,14 +77,23 @@ var searchEngine = {
   }
 
 }
-$(window).bind("load", function() {
-  var queryString = searchEngine.getQueryString('query');
-  searchEngine.populate();
-  searchEngine.perform(queryString);
-});
+
 window.onload = function() {
-    if(!window.location.hash) {
-        window.location = window.location + '#loaded';
-        window.location.reload();
+  if(window.location.pathname == "/search_results.html") {
+    var search_type = searchEngine.getQueryString('a')
+    if(search_type == 1) {
+      var queryString = searchEngine.getQueryString('query');
+      setTimeout(function() {
+        searchEngine.perform(queryString),
+        500
+      })
     }
+    if(search_type == 2) {
+      var queryString = searchEngine.getQueryString('tag',true);
+      setTimeout(function() {
+        searchEngine.perform(queryString),
+        500
+      })
+    }
+  }
 }
