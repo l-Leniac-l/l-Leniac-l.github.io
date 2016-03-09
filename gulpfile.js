36d3d4e8d7919a6f9bc8c2aaf5175c155157b642
Lenilson Nascimento <lenilson@Lenilson-Mac.local>
2016-03-09 01:22:48 -0300

    Change styles to stylus and postcss

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var browserSync = require('browser-sync').create(),
+    child = require('child_process');
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    stylus = require('gulp-stylus'),
+    nano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require("gulp-rename");
+
+var poststylus = require('poststylus'),
+    postcss = require('gulp-postcss'),
+    rucksack = require('rucksack-css'),
+    cssstats = require('postcss-cssstats');
+
+const siteRoot = '_site';
+const cssFiles = '_css/**/*.styl';
+
+gulp.task('stylus', () => {
+  gulp.src('_css/style.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+      use: [
+        poststylus([rucksack({autoprefixer: true})])
+      ]
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('minifyCss', () => {
+  gulp.src('assets/css/style.css')
+    .pipe(sourcemaps.init())
+    .pipe(nano())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['serve',
+    '--watch',
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('stylus-watch', ['stylus'], browserSync.reload);
+gulp.task('minify-watch', ['minifyCss'], browserSync.reload);
+
+gulp.task('serve', ['stylus','minifyCss'], () => {
+  browserSync.init({
+    browser: 'google chrome',
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+
+  gulp.watch(cssFiles, ['stylus-watch']);
+  gulp.watch('assets/css/style.css', ['minify-watch']);
+});
+
+gulp.task('cssstats', function() {
+    return gulp
+        .src('assets/css/style.min.css')
+        .pipe(
+            postcss([
+                cssstats(
+                    function(stats) {
+                        console.log(stats);
+                    }
+                )
+            ])
+        );
+});
+
+gulp.task('default', ['stylus','minifyCss','jekyll', 'serve']);
